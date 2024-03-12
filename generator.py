9c059d8d89c16e2d3a2e99b604520ea2537487ff
Natan Ayres <natan.nascimento.ayres@gmail.com>
2024-03-12 08:30:47 -0300

    Utilizando o Yield From
    Yield from puxa as pausas de outro generator

--- generator.py
+++ generator.py
@@ -10,3 +10,48 @@ def generator(v=0):
 gen = generator(v=0)
 for pausas in gen:
     print(pausas)
+
+def generator(n=0, maximum=10):
+    while True:
+        yield n
+        n += 1
+        
+        if n >= maximum:
+            return
+        
+gen = generator(maximum=100)
+for n in gen:
+    print(n)
+
+def gen1():
+    print("Começou Gen1")
+    yield 1
+    yield 2
+    yield 3
+    print("Acabou Gen1")
+
+def gen2():
+    print("Começou Gen2")
+    yield from gen1()
+    yield 4
+    yield 5
+    yield 6
+    print("Acabou Gen2")
+
+def gen3():
+    print("Começou Gen3")
+    yield 10
+    yield 20
+    yield 30
+    print("Acabou Gen3")
+
+
+g1 = gen1()
+g2 = gen2()
+g3 = gen3()
+for pausas in g1:
+    print(pausas)
+for pausas in g2:
+    print(pausas)
+for pausas in g3:
+    print(pausas)
