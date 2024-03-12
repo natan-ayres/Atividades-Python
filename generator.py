87b5cc7d9aec7753ee4b61d3514c4cb980f7ae6b
Natan Ayres <129100066+natan-ayres@users.noreply.github.com>
2024-03-12 08:18:55 -0300

    Testando com laços de repetição e com maximum

--- generator.py
+++ generator.py
@@ -10,3 +10,15 @@ def generator(v=0):
 gen = generator(v=0)
 for pausas in gen:
     print(pausas)
+
+def generator(n=0, maximum=10):
+    while True:
+        yield n
+        n += 1
+        
+        if n >= maximum:
+            return
+        
+gen = generator(maximum=1000)
+for n in gen:
+    print(n)
